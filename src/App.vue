1be8d1cf8ec40b1b8c878704e9948225fd19c95f
jhonarendra <jhonarendra@gmail.com>
2020-11-21 22:26:40 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <input v-model="name" type="text">
-  {{name}}
-  <button @click="counter++">Add increment</button>
-  {{counter}}
   <router-view/>
 </template>
 
@@ -14,8 +6,7 @@
   export default {
     data(){
       return {
-        name : 'Jhon',
-        counter: 0
+        
       }
     }
 
@@ -23,24 +14,27 @@
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  body {
+    background-color: #222b31;
+    margin: 0;
   }
-}
+
+  .m-0 {
+    margin: 0;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .card-shadow {
+    -webkit-box-shadow: 0px 5px 5px 0px rgba(8, 8, 8, 0.5);
+    -moz-box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.5);
+    box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.5);
+  }
 </style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+	<div id="navbar-wrap" class="card-shadow">
+		<div id="navbar">
+			<h2>Jhonarendra Movie</h2>
+			<div>
+				<input type="text" placeholder="Cari">
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+
+	}
+</script>
+
+<style lang='scss' scoped>
+	#navbar-wrap {
+		position: fixed;
+		width: 100%;
+		z-index: 9999;
+
+		#navbar {
+			display: flex;
+			padding: 15px;
+			justify-content: space-between;
+			background-color: #a34c01;
+
+			h2 {
+				margin: 0 1rem 0 0;
+				color: white;
+				cursor: pointer;
+			}
+
+			& > div {
+				display: flex;
+				flex-grow: 2;
+				justify-content: flex-end;
+
+				input {
+					width: 20%;
+					border: none;
+					height: 25px;
+					border-radius: 10px;
+					box-sizing: border-box;
+					outline: none;
+					padding: 5px;
+				}
+			}
+		}
+	}
+</style>
