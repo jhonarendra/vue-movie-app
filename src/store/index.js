4177a52dc2098c7e47f7912bcc4eeb54b9785c11
jhonarendra <jhonarendra@gmail.com>
2020-11-21 22:50:41 +0800

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import { createStore } from 'vuex'
 
+const SET_USER = 'SET_USER'
+
 export default createStore({
   state: {
+  	user: ()
   },
   mutations: {
+  	[SET_USER](state, user){
+  		state.user = user
+  	}
   },
   actions: {
+  	setUser({commit}, user){
+  		// call api
+
+  		commit(SET_USER, user)
+  	}
   },
   modules: {
+  },
+  getters: {
+  	isUserAdmin: state => {
+  		return state.user.role === 'admin'
+  	}
   }
 })
