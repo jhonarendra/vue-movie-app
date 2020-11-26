e625b393d7fda169e7fe7a816083f709a6ced284
jhonarendra <jhonarendra@gmail.com>
2020-11-26 13:13:23 +0800

    add firebase

---
+++ src/services/moviesApi.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase'
+
+const db = firebase.firestore().collection('movies')
+
+const moviesApi = {
+	getMovies: async() => {
+		const documents = await db.get()
+		return documents.docs.map(doc => {
+			return {
+				...doc.data(),
+				id: doc.id
+			}
+			// movie list yg diimport gak isi id 1 2 3 kayak tadi.
+
+		})
+	},
+	addMovie: async() => {
+
+	},
+	getMovieById: async() => {
+
+	},
+	updateMovies: async() => {
+
+	},
+	deleteMovies: async() => {
+
+	},
+}
+
+export default moviesApi
